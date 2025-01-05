8bac1270b46070351c7279311f19b4a5f89e4111
Kyle Open <Kyle4Open@gmail.com>
2025-01-05 16:48:11 +0900

    chore: "Local" 환경에대한 구성파일(appsetting.Local.json) 추가 및 IDE 론치환경을 Local로 변경, Local 환경에서도 Swagger 웹페이지가 나오도록 수정

--- src/MSD.Crux/MSD.Crux.API/Program.cs
+++ src/MSD.Crux/MSD.Crux.API/Program.cs
@@ -1,6 +1,7 @@
 /*******************
  * Web Host Builder
  *******************/
+
 var builder = WebApplication.CreateBuilder(args);
 
 /* DI Container   ******************/
@@ -16,10 +17,10 @@ var app = builder.Build();
 /*************************************************
  * HTTP request 파이프라인에 미들웨어 추가 및 설정
  *************************************************/
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsEnvironment("Local"))
 {
-  app.UseSwagger();
-  app.UseSwaggerUI();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
@@ -27,6 +28,8 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+Console.WriteLine($"실행중인 Environment: {app.Environment.EnvironmentName}");
 
 /***************
  * Run the host
