93714ca2b4619d195889a0d52a6fdbb42230b572
Kyle Open <Kyle4Open@gmail.com>
2025-01-05 14:30:38 +0900

    init: 포스트그레스 컨테이너: 테이블 칼럼들 글자수 제한 조정

--- docker/postgres/init.sql
+++ docker/postgres/init.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:286d2e8658e15c8f5c29e231f7f96f43c4c55759a05417afaf1b623ad5cbeae5
-size 3483
+oid sha256:537112d5a346d2481b5ef2b40fc655f3f8512c57b527bde64f57fbf413b1e1ba
+size 3616
