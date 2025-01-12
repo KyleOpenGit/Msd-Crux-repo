f763027f2031a71e7e6eaf19c4c16c87ec7e64bc
JoHB94 <webdp1503@gmail.com>
2025-01-12 16:43:16 +0900

    add LotService 추가

---
+++ src/MSD.Crux/MSD.Crux.Infra/Services/Default/LotService.cs
@@ -0,0 +1,14 @@
+﻿using MSD.Crux.Core.IRepositories;
+using MSD.Crux.Core.IServices;
+using MSD.Crux.Core.Models;
+
+namespace MSD.Crux.Infra.Services.Default
+{
+    public class LotService(ILotRepo _lotRepo) : ILotService
+    {
+        public async Task<List<Lot?>> GetAllCompletedLotsAsync()
+        {
+            return await _lotRepo.GetAllCompletedLotsAsync();
+        }
+    }
+}
