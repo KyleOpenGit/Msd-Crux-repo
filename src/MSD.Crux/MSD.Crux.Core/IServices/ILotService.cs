41cc14717ba5b02eb88e5d76dbd990763f6a1d0c
JoHB94 <webdp1503@gmail.com>
2025-01-12 16:42:19 +0900

    add Lot IService 추가

---
+++ src/MSD.Crux/MSD.Crux.Core/IServices/ILotService.cs
@@ -0,0 +1,13 @@
+﻿using MSD.Crux.Core.Models;
+
+namespace MSD.Crux.Core.IServices
+{
+    public interface ILotService
+    {
+        /// <summary>
+        /// 생산이 끝난 Lot를 조회
+        /// </summary>
+        /// <returns>조회된 로트정보 또는 null</returns>
+        Task<List<Lot?>> GetAllCompletedLotsAsync();
+    }
+}
