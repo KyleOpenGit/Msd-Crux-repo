a1200f28f6b26c0a9d30d4217e505d36413385d3
Kyle Open <Kyle4Open@gmail.com>
2025-01-14 00:21:19 +0900

    fix: 로컬 도커파일 유저 권한 추가

--- docker/postgres/init.sql
+++ docker/postgres/init.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:963e2ca1cc78b6f654014d6fa5f7fb8f4c0546b03de11943e6c64331212fa456
-size 4202
+oid sha256:8884aa09f67ec417b8b81321a30133d48e61ef6f4ef1c55131de347276a415df
+size 4554
