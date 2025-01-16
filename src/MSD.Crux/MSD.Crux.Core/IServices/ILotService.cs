057a1fdcd5eb6bd98d453f0155eb900db4a17124
Kyle Open <Kyle4Open@gmail.com>
2025-01-16 21:35:13 +0900

    feat: 새로운 Lot Id 발급 API 엔드포인트 추가 [/api/lot/issue-new]

--- src/MSD.Crux/MSD.Crux.Core/IServices/ILotService.cs
+++ src/MSD.Crux/MSD.Crux.Core/IServices/ILotService.cs
@@ -1,13 +1,22 @@
 ﻿using MSD.Crux.Core.Models;
 
-namespace MSD.Crux.Core.IServices
+namespace MSD.Crux.Core.IServices;
+
+/// <summary>
+/// Lot 테이블 관련 서비스 구현체
+/// </summary>
+public interface ILotService
 {
-    public interface ILotService
-    {
-        /// <summary>
-        /// 생산이 끝난 Lot를 조회
-        /// </summary>
-        /// <returns>조회된 로트정보 또는 null</returns>
-        Task<List<Lot?>> GetAllCompletedLotsAsync();
-    }
+    /// <summary>
+    /// 생산이 끝난 Lot를 조회
+    /// </summary>
+    /// <returns>조회된 로트정보 또는 null</returns>
+    Task<List<Lot?>> GetAllCompletedLotsAsync();
+
+    /// <summary>
+    /// 새로운 Lot ID 발급
+    /// </summary>
+    /// <param name="request">Lot ID 발급 요청 DTO</param>
+    /// <returns>새로 발급된 Lot ID</returns>
+    Task<string> IssueNewLotIdAsync(LotIdIssueReqDto request);
 }
