fb6b6b55ec01e465436b8647a1a8b19fbbac9a65
Kyle Open <Kyle4Open@gmail.com>
2025-01-16 10:48:00 +0900

    add: 생산계획 테이블(injection_plan) 레포지토리 추가

--- docker/postgres/init.sql
+++ docker/postgres/init.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ad211573e0a68a8b844f83f90e2ed463c14083d2853ef54b4b283cc061aba7f7
-size 5134
+oid sha256:94d1e0dc97f8e6a07194e097066dab2f41a43f7c08dcbd4785566f017c03f235
+size 5140
---
+++ src/MSD.Crux/MSD.Crux.Infra/Repositories/InMemory/InjectionPlanRepoInMemory.cs
@@ -0,0 +1,70 @@
+using System.Collections.Concurrent;
+using MSD.Crux.Core.IRepositories;
+using MSD.Crux.Core.Models;
+
+namespace MSD.Crux.Infra.Repositories;
+
+/// <summary>
+/// injection_plan 테이블 In-Memory 레포지토리 구현체
+/// </summary>
+public class InjectionPlanRepoInMemory : IInjectionPlanRepo
+{
+    private readonly ConcurrentDictionary<(string, DateTime), InjectionPlan> _plans = new();
+
+    public Task<InjectionPlan?> GetByPartAndDateAsync(string partId, DateTime date)
+    {
+        _plans.TryGetValue((partId, date), out var plan);
+        return Task.FromResult(plan);
+    }
+
+    public Task<IEnumerable<InjectionPlan>> GetByWeekAsync(int weekNumber)
+    {
+        var plans = _plans.Values.Where(p => p.IsoWeek == weekNumber);
+        return Task.FromResult(plans.AsEnumerable());
+    }
+
+    public Task AddAsync(InjectionPlan plan)
+    {
+        if (!_plans.TryAdd((plan.PartId, plan.Date), plan))
+        {
+            throw new InvalidOperationException($"Plan for part {plan.PartId} on {plan.Date:yyyy-MM-dd} already exists.");
+        }
+
+        return Task.CompletedTask;
+    }
+
+    public Task UpdateAsync(InjectionPlan plan)
+    {
+        if (!_plans.ContainsKey((plan.PartId, plan.Date)))
+        {
+            throw new InvalidOperationException($"Plan for part {plan.PartId} on {plan.Date:yyyy-MM-dd} does not exist.");
+        }
+
+        _plans[(plan.PartId, plan.Date)] = plan;
+        return Task.CompletedTask;
+    }
+
+    public Task DeleteAsync(string partId, DateTime date)
+    {
+        if (!_plans.TryRemove((partId, date), out _))
+        {
+            throw new InvalidOperationException($"Plan for part {partId} on {date:yyyy-MM-dd} does not exist.");
+        }
+
+        return Task.CompletedTask;
+    }
+
+    public Task<IEnumerable<IGrouping<string, InjectionPlan>>> GetGroupedByPartAndOrderedByDateAsync(int weekNumber)
+    {
+        var grouped = _plans.Values.Where(p => p.IsoWeek == weekNumber).OrderBy(p => p.Date).GroupBy(p => p.PartId);
+
+        return Task.FromResult(grouped.AsEnumerable());
+    }
+
+    public Task<Dictionary<string, int>> GetDailyQuantitiesByPartAsync(DateTime date)
+    {
+        var result = _plans.Values.Where(p => p.Date.Date == date.Date).GroupBy(p => p.PartId).ToDictionary(g => g.Key, g => g.Sum(p => p.QtyDaily));
+
+        return Task.FromResult(result);
+    }
+}
