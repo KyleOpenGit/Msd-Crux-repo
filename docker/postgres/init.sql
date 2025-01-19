26498f39eab8386b856eedfc03547cbf8c729b22
Kyle Open <Kyle4Open@gmail.com>
2025-01-19 16:27:39 +0900

    chore: DB 초기데이터 생산계획 수량 변경

--- docker/postgres/init.sql
+++ docker/postgres/init.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2970af91e69f61a31657b3371687c319a26deb8a6e59061dd5bbfbc87ea0a830
-size 44937
+oid sha256:6f111a79f8edbdce5a3184a7861bd1b669ebd51d027751ece2ee3b7e41af83f5
+size 45170
