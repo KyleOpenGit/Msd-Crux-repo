e0913ffbfb2d9e53eff65bde97965372112e1ce8
Kyle Open <Kyle4Open@gmail.com>
2025-01-19 19:04:57 +0900

    fix: Lot 넘버 발급시 요청된 PartId가 part 테이블에 id가 있어야만 발급하도록 수정

--- src/MSD.Crux/MSD.Crux.Infra/Services/Default/LotService.cs
+++ src/MSD.Crux/MSD.Crux.Infra/Services/Default/LotService.cs
@@ -4,7 +4,7 @@ using MSD.Crux.Core.Models;
 
 namespace MSD.Crux.Infra.Services;
 
-public class LotService(ILotRepo _lotRepo) : ILotService
+public class LotService(ILotRepo _lotRepo, IPartRepo _partRepo) : ILotService
 {
     public async Task<List<Lot?>> GetAllCompletedLotsAsync()
     {
@@ -13,16 +13,31 @@ public class LotService(ILotRepo _lotRepo) : ILotService
 
     public async Task<string> IssueNewLotIdAsync(LotIdIssueReqDto request)
     {
+        // PartId 검증
+        if (!await IsPartIdValidAsync(request.PartId))
+        {
+            throw new ArgumentException($"Invalid PartId: {request.PartId}는 존재하지 않습니다.");
+        }
+
         string prefix = $"{request.PartId}-{request.Date:yyyyMMdd}-";
 
         // 가장 최신 순번 조회 후 새로운 Lot ID 생성
-        int latestSequence = await _lotRepo.GetLatestSequenceOfIdAsync(request.PartId, (request.Date));
-
+        int latestSequence = await _lotRepo.GetLatestSequenceOfIdAsync(request.PartId, request.Date);
         string newLotId = $"{prefix}{latestSequence + 1}";
 
         // 새로운 Lot 최소 정보 추가
         await _lotRepo.AddMinimalAsync(new Lot { Id = newLotId, PartId = request.PartId, LineId = request.LineId, IssuedTime = request.Date });
 
         return newLotId;
+    }
+
+    /// <summary>
+    /// PartId 유효성 검증.
+    /// </summary>
+    /// <param name="partId">검증할 PartId</param>
+    /// <returns>테이블 레코드 존재 여부</returns>
+    private async Task<bool> IsPartIdValidAsync(string partId)
+    {
+        return await _partRepo.ExistsByIdAsync(partId);
     }
 }
