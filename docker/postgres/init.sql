008af16d3f81cdaf63306bc69d4346786b751e4b
Kyle Open <Kyle4Open@gmail.com>
2025-01-19 21:29:38 +0900

    lot 테이블 레코드 12개 시딩

--- docker/postgres/init.sql
+++ docker/postgres/init.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6f111a79f8edbdce5a3184a7861bd1b669ebd51d027751ece2ee3b7e41af83f5
-size 45170
+oid sha256:3abca9ad9900599389c9e7db85511fd72d636da46e4e87e130b0dcfcb1789d87
+size 51840
